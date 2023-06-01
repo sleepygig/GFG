389c9d8053f790c04d74f4ea297ac60379e8e24b
Aditya Kumar <aditya220972@gmail.com>
2023-06-02 01:38:03 +0530

    Added solution - LeetHub

--- Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.cpp
+++ Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.cpp
@@ -10,47 +10,48 @@ using namespace std;
 
 class Solution {
   public:
- int dr[8] = {-1, 0, 1, 0, -1, -1, 1, 1};
+int dr[8] = {-1, 0, 1, 0, -1, -1, 1, 1};
 int dc[8] = {0, 1, 0, -1, -1, 1, -1, 1};
-
-   int shortestPath(vector<vector<int>> &grid, pair<int, int> source,pair<int, int> destination) {
-          int n=grid.size();
-          int m=grid[0].size();
-          queue<pair<pair<int,int>,int>>q;   //{ { i,j} dis}
-          q.push({source,0});
-          int vis[n][m];
-          int y=INT16_MAX;
-          vis[source.first][source.second]=1;
-          memset(vis,0,sizeof(vis));
-          while(q.size())
+       int shortestPath(vector<vector<int>> &grid, pair<int, int> source,pair<int, int> destination) {
+        int n=grid.size();
+        int m=grid[0].size();
+       #define pi pair<int,int>
+      //  int dis[n][m];
+      //  memset(dis,INT16_MAX,sizeof(dis));
+       vector<vector<int>>dis(n,vector<int>(m,1e8));
+       dis[source.first][source.second]=0;
+      //  cout<<dis[destination.first][destination.second];
+       priority_queue<pair<int,pi>, vector<pair<int,pi>>, greater<pair<int,pi>>>q;
+       q.push({dis[source.first][source.second],source});
+       while ((q.size()))
+       {
+        int tr=q.top().second.first;
+        int tc=q.top().second.second;
+        q.pop();
+          for(int i=0;i<4;i++)
           {
-            // cout<<"g"<<endl;
-            int tr=q.front().first.first;
-            int tc=q.front().first.second;
-            int dis=q.front().second;
-            
-           if (tr == destination.first && tc == destination.second) {
-            y = min(y, dis);
-            return y;
-        }
-            q.pop();
-            for(int i=0;i<4;i++)
-            {
-                int nr=tr+dr[i];
-                int nc=tc+dc[i];
-                if(nr>=0 and nr<n and nc>=0 and nc<m)
+              int nr=tr+dr[i];
+              int nc=tc+dc[i];
+              if(nc>=0 and nc<m and nr>=0 and nr<n)
+              {
+
+                if(grid[nr][nc]==1 and dis[nr][nc]>dis[tr][tc]+1)
                 {
-                  if(grid[nr][nc]==1 and vis[nr][nc]==0)
-                  {
-                      vis[nr][nc]=1;
-                    q.push({{nr,nc},dis+1});
-                  }
+                  dis[nr][nc]=dis[tr][tc]+1;   //relaxing of edge 
+                  q.push({dis[nr][nc],{nr,nc}});
                 }
-            }
+              }
           }
-          if(y==INT16_MAX) return -1;
-          // deb(y);
-return y;        
+       }
+       if(dis[destination.first][destination.second]==1e8)
+       {
+       return -1;
+       }
+       else
+       {
+        return dis[destination.first][destination.second];
+       }
+      
     }
 };
 
