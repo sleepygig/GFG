c9f5871b8a0922c15942041ef2cfa89772a4e162
Aditya Kumar <aditya220972@gmail.com>
2023-06-17 10:49:05 +0530

    Added solution - LeetHub

---
+++ Nodes at given distance in binary tree - GFG/nodes-at-given-distance-in-binary-tree.cpp
@@ -0,0 +1,178 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+/* A binary Tree node
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+};
+*/
+
+class Solution
+{
+private:
+void treetoGr(Node* root,map<int ,vector<int>>&adj)
+{
+  if(!root) return ;
+  int par=root->data;
+  if(root->left) {
+    int l=root->left->data;
+    adj[l].push_back(par);
+    adj[par].push_back(l);
+      treetoGr(root->left,adj);
+  }
+  
+  if(root->right) {
+    int r=root->right->data;
+    adj[r].push_back(par);
+    adj[par].push_back(r);
+      treetoGr(root->right,adj);
+  }
+}
+void bfs(map<int ,vector<int>>&adj,map<int,int>&vis,int k,int src,vector<int>&ans)
+{
+    queue<pair<int,int>>q;   //node //lvl
+    q.push({src,0});
+    while(q.size())
+    {
+        int tp=q.front().first;
+        int lv=q.front().second;
+        vis[tp]=1;
+        if(lv==k) ans.push_back(tp);
+        q.pop();
+        for(auto c:adj[tp])
+        {if(!vis[c]) q.push({c,lv+1});}
+    }
+}
+
+public:
+
+    vector <int> KDistanceNodes(Node* root, int target , int k)
+    {int y=0;
+    map<int,vector<int>>adj;
+    treetoGr(root,adj);
+    map<int,int>vis;
+    vector<int>ans;
+    bfs(adj,vis,k,target,ans);
+    sort(ans.begin(),ans.end());
+    return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    getchar();
+    
+    Solution x = Solution();
+    
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* head = buildTree(s);
+        
+        int target, k;
+        cin>> target >> k;
+        getchar();
+        
+        vector <int> res = x.KDistanceNodes(head, target, k);
+        
+        for( int i=0; i<res.size(); i++ )
+            cout<< res[i] << " ";
+        cout<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
